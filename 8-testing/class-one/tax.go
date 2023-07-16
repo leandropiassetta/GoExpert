c51f6bd944bfdc1627508d994c5aa3ba49ed961d
lpiasseta <leandro.piasseta@mercadolivre.com>
2023-07-16 18:21:15 -0300

    add benchmark

--- 8-testing/class-one/tax.go
+++ 8-testing/class-one/tax.go
@@ -1,6 +1,25 @@
 package tax
 
+import "time"
+
 func CalculateTax(amount float64) float64 {
+	tax := 5.0
+
+	if amount == 0 {
+		tax = 0.0
+		return tax
+	}
+
+	if amount >= 1000 {
+		tax = 10.0
+		return tax
+	}
+
+	return tax
+}
+
+func CalculateTax_2(amount float64) float64 {
+	time.Sleep(time.Millisecond)
 	tax := 5.0
 
 	if amount == 0 {
--- 8-testing/class-one/tax_test.go
+++ 8-testing/class-one/tax_test.go
@@ -36,3 +36,31 @@ func Test_CalculateTax_Batch(t *testing.T) {
 		}
 	}
 }
+
+// benchmark -> test the performance of the code (how long it takes to run) -> go test -bench=. -> run all benchmarks in the file (the name of the function must start with Benchmark) -> go test -bench=BenchMarkCalculateTax -> run a specific benchmark in the file (the name of the function must start with Benchmark) -> go test -bench=BenchMarkCalculateTax -benchtime=10s -> run a specific benchmark in the file (the name of the function must start with Benchmark) for 10 seconds
+
+// go test -bench=.   -> run all benchmarks in the file (the name of the function must start with Benchmark)
+
+// go test -bench=. -run=^#  -> run all benchmarks in the file (the name of the function must start with Benchmark) and ignore the tests (the name of the function must start with Test)
+
+// to have that write "Benchmark" + the name of the function that you want to test
+func BenchmarkCalculateTax(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500)
+	}
+}
+
+// verifing the performance of the function CalculateTax_2 (it takes more time to run than the function CalculateTax), benchmarking is a good way to find out if the code is running well or not and compare the performance of the code
+func BenchmarkCalculateTax_2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax_2(500)
+	}
+}
+
+// go test -bench=. -run=^# -count=10 -> run all benchmarks in the file (the name of the function must start with Benchmark) and ignore the tests (the name of the function must start with Test) and run 10 times (the default is 1 time)
+
+// go test -bench=. -run=^# -count=10 -benchtime=3s --> run all benchmarks in the file (the name of the function must start with Benchmark) and ignore the tests (the name of the function must start with Test) and run 10 times for 3 seconds each time (the default is 1 second)
+
+// go help test -> to see all the options that you can use with go test command (go help testflag -> to see all the options that you can use with go test command)
+
+//  go test -bench=. -run=^# -benchmem -> to see the memory allocation of the function CalculateTax_2 (it allocates more memory than the function CalculateTax) -> go test -bench=. -run=^# -benchmem -memprofile=mem.out -> to see the memory allocation of the function CalculateTax_2 (it allocates more memory than the function CalculateTax) and save the result in a file called mem.out
