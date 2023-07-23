af1e08bb73a1615360402b8e8f15127e8ba76342
lpiasseta <leandro.piasseta@mercadolivre.com>
2023-07-23 17:49:10 -0300

    finalizing create user

--- 9-apis/internal/infra/database/user_db_test.go
+++ 9-apis/internal/infra/database/user_db_test.go
@@ -26,7 +26,6 @@ func Test_CreateUser(t *testing.T) {
 	userDB := NewUser(db)
 
 	err = userDB.CreateUser(user)
-
 	assert.Nil(t, err)
 
 	var userFound *entity.User
@@ -37,4 +36,26 @@ func Test_CreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.Equal(t, user.Name, userFound.Name)
 	assert.Equal(t, user.ID, userFound.ID)
+}
+
+func Test_FindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	db.AutoMigrate(&entity.User{})
+
+	user, _ := entity.NewUser("John", "j@j.com", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.CreateUser(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Password, userFound.Password)
 }
