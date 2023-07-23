e9a77a3d51300d3325ff72b2c99a7d8d9f0ae0b0
lpiasseta <leandro.piasseta@mercadolivre.com>
2023-07-23 16:53:04 -0300

    testing product

--- 9-apis/internal/entity/product/product_test.go
+++ 9-apis/internal/entity/product/product_test.go
@@ -1,3 +1,48 @@
 package product_test
 
-func main() {}
+import (
+	"testing"
+
+	product "github.com/leandropiassetta/goexpert/9-apis/internal/entity/product"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewProduct(t *testing.T) {
+	p, err := product.NewProduct("Product 1", 100)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.ID)
+	assert.Equal(t, "Product 1", p.Name)
+	assert.Equal(t, 100, p.Price)
+	assert.NotEmpty(t, p.CreatedAt)
+}
+
+func Test_ProductWhenNameIsRequired(t *testing.T) {
+	p, err := product.NewProduct("", 100)
+
+	assert.Nil(t, p)
+	assert.Equal(t, product.ErrorNameIsRequired, err)
+}
+
+func Test_ProductWhenPriceIsRequired(t *testing.T) {
+	p, err := product.NewProduct("Product 1", 0)
+
+	assert.Nil(t, p)
+	assert.Equal(t, product.ErrorPriceIsRequired, err)
+}
+
+func Test_ProductWhenPriceIsInvalid(t *testing.T) {
+	p, err := product.NewProduct("Product 1", -1)
+
+	assert.Nil(t, p)
+	assert.Equal(t, product.ErrorInvalidPrice, err)
+}
+
+func Test_ProductValidate(t *testing.T) {
+	p, err := product.NewProduct("Product 1", 10)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.Nil(t, p.Validate())
+}
