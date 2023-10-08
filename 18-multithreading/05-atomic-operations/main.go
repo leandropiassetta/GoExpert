6443af0ca382235f87201937ad38dc2b9d5a2335
lpiasseta <leandro.piasseta@mercadolivre.com>
2023-10-08 17:08:29 -0300

    adding atomic operations

--- 18-multithreading/05-atomic-operations/main.go
+++ 18-multithreading/05-atomic-operations/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
 var number uint64
 
+// ATOMIC OPERATIONS: Are operations that are executed in a single step, without the possibility of interruption.
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		number++
+		// number++
+
+		// In this package make internal operations to avoid the interruption of the goroutines.
+		atomic.AddUint64(&number, 1)
 
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte("You is the visitor number " + fmt.Sprint(number) + "\n"))
